0b5bea121d11baaa1a59f6cd2bdf1f6ba74eec18
Denis <jackrassul@icloud.com>
2026-06-21 16:41:28 +0500

    chore: fix config settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,64 +1,32 @@
 import js from '@eslint/js';
-import prettierConfig from 'eslint-config-prettier/flat';
-import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import unicorn from 'eslint-plugin-unicorn';
+import globals from 'globals';
 
-export default [
+export default defineConfig(
   {
-    ignores: ['dist', 'node_modules', 'public', 'temp', '.vscode', 'vite.config.js'],
+    ignores: ['dist/**', 'node_modules/**', '.vite/**'],
   },
 
   js.configs.recommended,
-  prettierConfig,
+  unicorn.configs.recommended,
 
   {
-    files: ['**/*.js'],
-    plugins: { unicorn: unicorn },
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
         ...globals.browser,
         ...globals.node,
       },
-      ecmaVersion: 'latest',
-      sourceType: 'module',
     },
     rules: {
-      camelcase: 'error',
-      'no-unused-vars': 'warn',
-      'no-undef': 'error',
-      'prefer-const': 'error',
-      'no-var': 'error',
-      eqeqeq: ['error', 'always'],
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-      curly: ['error', 'all'],
-      'new-cap': [
-        'error',
-        {
-          newIsCap: true,
-          capIsNew: false,
-        },
-      ],
-
-      //unicorn
-      'unicorn/prefer-includes': 'error',
-      'unicorn/prefer-array-some': 'error',
-      'unicorn/prefer-array-find': 'error',
-      'unicorn/prefer-string-replace-all': 'error',
-
-      'unicorn/no-new-array': 'error',
-      'unicorn/no-array-push-push': 'error',
-      'unicorn/no-unnecessary-await': 'error',
-      'unicorn/no-zero-fractions': 'error',
-      'unicorn/number-literal-case': 'error',
-      'unicorn/escape-case': 'error',
-
-      'unicorn/consistent-function-scoping': 'warn',
-      'unicorn/prefer-ternary': 'warn',
-
       'unicorn/prevent-abbreviations': 'off',
       'unicorn/filename-case': 'off',
-      'unicorn/no-null': 'off',
-      'unicorn/no-array-reduce': 'off',
     },
   },
-];
+
+  eslintConfigPrettier,
+);
